d301fca2b1c789d568a1ed0f1f59389023797070
karthik01932 <karthiksrinivas2001@gmail.com>
2024-05-15 13:13:13 +0530

    first commit

---
+++ public/signup.js
@@ -0,0 +1,12 @@
+async function signup(event){
+    try {
+        event.preventDefault();
+
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const phone = event.traget.phone.value; 
+        const password = event.target.password.value;
+    } catch (error) {
+        
+    }
+}
